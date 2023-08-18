07c6938b4f50ec5e6afcf0a3ffeefd92927b6f68
ddimkou <ddimkou@outlook.com>
2023-08-18 22:58:16 +0300

    architecture

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92.9vh" },
+          borderRight: "1px solid #4d4d4d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "orange" }}
+        >
+          Copyright 2023 ddimkou
+        </Typography>
+      </Box>
+      <Box>Main feed</Box>
+    </Stack>
+  );
 };
 
 export default Feed;
