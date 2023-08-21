22add3d3f0dc383b9343838f4f8e46e3c9c19496
ddimkou <ddimkou@outlook.com>
2023-08-21 14:36:50 +0300

    sidebar scrolling on small devices

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,6 +26,37 @@
 
 // export default Sidebar;
 
+// import { Stack } from "@mui/material";
+
+// const Sidebar = () => {
+//   return (
+//     <Stack
+//       direction="row"
+//       sx={{
+//         position: "sticky",
+//         top: 0,
+//         zIndex: 1,
+//         overflowY: "none",
+//         textAlign: "center",
+//         height: { sx: "auto", md: "100vh" },
+//         flexDirection: {
+//           md: "column",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         },
+//       }}
+//     >
+//       <div>HORRORHORROR</div>
+//       <div>2HORROR</div>
+//       <div>3HORRORHORROR</div>
+//       <div>4HORRORHORROR</div>
+//       <div>5HORROR</div>
+//     </Stack>
+//   );
+// };
+
+// export default Sidebar;
+
 import { Stack } from "@mui/material";
 
 const Sidebar = () => {
@@ -36,13 +67,20 @@ const Sidebar = () => {
         position: "sticky",
         top: 0,
         zIndex: 1,
-        overflowY: "none",
+        overflowY: "auto",
         textAlign: "center",
         height: { sx: "auto", md: "100vh" },
         flexDirection: {
+          xs: "row",
           md: "column",
-          justifyContent: "center",
-          alignItems: "center",
+        },
+        justifyContent: {
+          xs: "flex-start",
+          md: "center",
+        },
+        alignItems: {
+          xs: "center",
+          md: "center",
         },
       }}
     >
