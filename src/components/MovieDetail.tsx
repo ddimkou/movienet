0d649eee5b7d86ca8cea95fcac1d4ac68025e1f7
ddimkou <ddimkou@outlook.com>
2023-08-21 18:00:51 +0300

    movie details

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Movie } from "../hooks/useMovies";
+import fetchRating from "../hooks/fetchRating";
+import { Stack, Typography } from "@mui/material";
 
-const MovieDetail = () => {
-  return <div>MovieDetail</div>;
+interface Props {
+  movie: Movie;
+}
+const MovieDetail = ({ movie }: Props) => {
+  const [rating, setRating] = useState<number | null>(null);
+  useEffect(() => {
+    const fetchMovieRating = async () => {
+      const fetchedRating = await fetchRating(movie.id);
+      setRating(fetchedRating);
+    };
+
+    fetchMovieRating();
+  }, [movie.id]);
+  return (
+    <Stack direction="row" justifyContent="space-between" sx={{ pt: "1em" }}>
+      <Typography
+        sx={{
+          fontWeight: "bold",
+        }}
+      >
+        {movie?.releaseYear?.year}
+      </Typography>
+      <Typography
+        sx={{
+          backgroundColor:
+            rating !== null
+              ? rating > 9
+                ? "rgba(0, 255, 0, 0.3)" // green 30%opacity
+                : rating >= 7.5
+                ? "rgba(255, 165, 0, 0.3)" // orange 30% opacity
+                : "rgba(255, 0, 0, 0.3)" // red 30% opacity
+              : "transparent",
+          display: "inline-block",
+          padding: "0.2em 0.5em",
+          borderRadius: "4px",
+        }}
+      >
+        {rating !== null ? rating.toFixed(1) : "N/A"}
+      </Typography>
+    </Stack>
+  );
 };
 
 export default MovieDetail;
--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,9 +1,37 @@
-import { Stack } from "@mui/material";
+// import { Stack } from "@mui/material";
+// import useMovies from "../hooks/useMovies";
+// import MovieCard from "./MovieCard";
+
+// const MovieGrid = () => {
+//   const { movies, error } = useMovies();
+
+//   return (
+//     <Stack
+//       direction="row"
+//       flexWrap="wrap"
+//       justifyContent="center"
+//       alignItems="center"
+//       gap={3}
+//       pt={8}
+//     >
+//       {error ? (
+//         <div>Error: {error}</div>
+//       ) : (
+//         movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+//       )}
+//     </Stack>
+//   );
+// };
+
+// export default MovieGrid;
+
+import { Stack, Skeleton } from "@mui/material";
 import useMovies from "../hooks/useMovies";
 import MovieCard from "./MovieCard";
 
 const MovieGrid = () => {
   const { movies, error } = useMovies();
+  const isLoading = movies.length === 0 && !error; // Determine loading state
 
   return (
     <Stack
@@ -14,7 +42,17 @@ const MovieGrid = () => {
       gap={3}
       pt={8}
     >
-      {error ? (
+      {isLoading ? (
+        Array.from({ length: 10 }).map((_, index) => (
+          <Skeleton
+            key={index}
+            variant="circular"
+            width={320}
+            height={446}
+            sx={{ borderRadius: "4%", bgcolor: "grey.600" }}
+          />
+        ))
+      ) : error ? (
         <div>Error: {error}</div>
       ) : (
         movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
