c0c64672d0440e5dada0e8c3746c98fe184a201c
ddimkou <ddimkou@outlook.com>
2023-08-24 12:26:24 +0300

    display the titles

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,19 +1,20 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
-// import MovieGrid from "./MovieGrid";
 import { useState } from "react";
-// import Test from "./Test";
+import useMoviesByGenre from "../hooks/useMoviesByGenre";
+import MoviesDisplay from "./MoviesDisplay";
 
 const Feed = () => {
   const [selectedGenre, setSelectedGenre] = useState<string | null>(null);
-  // const [moviesDisplay, setMoviesDisplay] = useState([]);
+
+  // Create a Genre object from the selectedGenre string
+  const genreFilter = selectedGenre ? { genre: selectedGenre } : null;
+
+  // Call the hook with the genreFilter object
+  const { genreSearch } = useMoviesByGenre(genreFilter);
 
   return (
-    <Stack
-      direction={{ xs: "column", md: "row" }}
-      height="auto"
-      // overflow="hidden"
-    >
+    <Stack direction={{ xs: "column", md: "row" }} height="auto">
       <Box
         sx={{
           borderRight: "1px solid #FFF6E0",
@@ -24,18 +25,13 @@ const Feed = () => {
           selectedGenre={selectedGenre}
           setSelectedGenre={setSelectedGenre}
         />
-        {/* <Test></Test> */}
       </Box>
-      <Box
-        p={2}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          // overflowY: "scroll",
-          flex: 1,
-        }}
-      >
-        {/* <MovieGrid /> */}
+      <Box p={2} sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+        {selectedGenre && genreSearch.length > 0 ? (
+          <MoviesDisplay movies={genreSearch} /> // Pass genreSearch array to MoviesDisplay
+        ) : (
+          <Typography variant="h6">Select a genre to see movies</Typography>
+        )}
       </Box>
     </Stack>
   );
---
+++ src/components/MoviesDisplay.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface Movie {
+  id: string;
+  titleText: {
+    text: string;
+  };
+  primaryImage: {
+    url: string;
+  };
+  releaseYear: {
+    year: number;
+  };
+}
+
+interface MoviesDisplayProps {
+  movies: Movie[];
+}
+
+const MoviesDisplay: React.FC<MoviesDisplayProps> = ({ movies }) => {
+  const movieNames = movies.map((movie) => movie.titleText.text);
+
+  return (
+    <div>
+      {movieNames.map((name, index) => (
+        <p key={index}>{name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default MoviesDisplay;
