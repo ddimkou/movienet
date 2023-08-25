524d4f1ced341c0274e8d233c96bafb13f403ac9
ddimkou <ddimkou@outlook.com>
2023-08-25 14:50:10 +0300

    genres

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import apiKey from "./apiKey";
+import apiKey from "../services/apiKey";
 
 const useMovies = axios.create({
   baseURL: "https://api.themoviedb.org/3/movie",
--- src/services/api-client.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://moviesdatabase.p.rapidapi.com",
-  headers: {
-    "X-RapidAPI-Key": "8297b97458msha610e9f13c73523p1d60c7jsn8a3fe9c7e4fc",
-    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-  },
-});
