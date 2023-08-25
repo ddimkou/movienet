97962d4ea29d7b77f2a24cc6ef1c874ed1579224
ddimkou <ddimkou@outlook.com>
2023-08-25 14:40:16 +0300

    update ignore

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import apiKey from "./apiKey";
+
 const useMovies = axios.create({
   baseURL: "https://api.themoviedb.org/3/movie",
   params: {
     api_key: apiKey,
   },
 });
+
 export default useMovies;
