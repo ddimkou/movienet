14b6c0cba91e696ec091fd2c6b404792fa4b8d19
ddimkou <ddimkou@outlook.com>
2023-08-28 11:13:38 +0300

    critic score

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,4 +1,36 @@
-import { Card, CardContent, CardMedia } from "@mui/material";
+// import { Card, CardContent, CardMedia } from "@mui/material";
+// import { Movie } from "../hooks/useMovies";
+// import Typography from "@mui/material/Typography";
+
+// interface Props {
+//   movie: Movie;
+// }
+// const MovieCard = ({ movie }: Props) => {
+//   const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+//   return (
+//     <Card className="movie-card" sx={{ backgroundColor: "#272829" }}>
+//       <CardMedia
+//         component="img"
+//         height="400px"
+//         width="300px"
+//         image={imageUrl}
+//         alt={movie.title}
+//       />
+//       <CardContent>
+//         <Typography color="white" variant="h6" gutterBottom textAlign="center">
+//           {movie.title}
+//         </Typography>
+//         {/* <Typography variant="body2" color="textSecondary">
+//           {movie.overview}
+//         </Typography> */}
+//       </CardContent>
+//     </Card>
+//   );
+// };
+
+// export default MovieCard;
+import { Box, Card, CardMedia } from "@mui/material";
 import { Movie } from "../hooks/useMovies";
 import Typography from "@mui/material/Typography";
 
@@ -9,7 +41,7 @@ const MovieCard = ({ movie }: Props) => {
   const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
-    <Card className="movie-card">
+    <Card className="movie-card" sx={{ backgroundColor: "#272829" }}>
       <CardMedia
         component="img"
         height="400px"
@@ -17,14 +49,22 @@ const MovieCard = ({ movie }: Props) => {
         image={imageUrl}
         alt={movie.title}
       />
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
+      <Box sx={{ position: "absolute", top: 0, right: 0 }}>
+        <Typography
+          color="white"
+          variant="body2"
+          textAlign="center"
+          bgcolor="rgba(0, 0, 0, 0.6)"
+          p={1}
+        >
+          {movie.vote_average}
+        </Typography>
+      </Box>
+      <div className="overlay">
+        <Typography color="white" variant="h6" textAlign="center">
           {movie.title}
         </Typography>
-        {/* <Typography variant="body2" color="textSecondary">
-          {movie.overview}
-        </Typography> */}
-      </CardContent>
+      </div>
     </Card>
   );
 };
