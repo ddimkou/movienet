2bd27524654af857c6c9b2dc3109e6d4be55b840
ddimkou <ddimkou@outlook.com>
2023-08-31 01:30:15 +0300

    genre prop fixes

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,37 +1,12 @@
-// import { Box, Stack } from "@mui/material";
-// import Sidebar from "./Sidebar";
-// import MovieGrid from "./MovieGrid";
-
-// const Feed = () => {
-//   return (
-//     <Stack direction={{ xs: "column", md: "row" }} height="auto">
-//       <Box
-//         sx={{
-//           borderRight: "1px solid #FFF6E0",
-//           px: { xs: 0, md: 2 },
-//         }}
-//       >
-//         <Sidebar />
-//       </Box>
-//       <Box p={2} sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
-//         <MovieGrid />
-//       </Box>
-//     </Stack>
-//   );
-// };
-
-// export default Feed;
-
 import { Box, Stack } from "@mui/material";
 import Sidebar from "./Sidebar";
 import MovieGrid from "./MovieGrid";
 import { useState } from "react";
 
 const Feed = () => {
-  const [selectedOption, setSelectedOption] = useState<
-    string | number | undefined
-  >("popular");
-
+  const [selectedOption, setSelectedOption] = useState<string | number>(
+    "popular"
+  );
   return (
     <Stack direction={{ xs: "column", md: "row" }} height="auto">
       <Box
@@ -42,7 +17,7 @@ const Feed = () => {
       >
         <Sidebar
           selectedOption={selectedOption}
-          onSelectOption={setSelectedOption}
+          setSelectedOption={setSelectedOption}
         />
       </Box>
       <Box p={2} sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,36 +1,9 @@
-// import { Stack, Typography } from "@mui/material";
-
-// import useMovies from "../hooks/useMovies";
-// import MovieCard from "./MovieCard";
-
-// const MovieGrid = () => {
-//   const { movies, error } = useMovies("/movie/popular"); // Pass the desired endpoint here
-
-//   return (
-//     <Stack
-//       direction="row"
-//       flexWrap="wrap"
-//       justifyContent="center"
-//       alignItems="center"
-//       gap={3}
-//       pt={8}
-//     >
-//       {error && <Typography>{error}</Typography>}
-//       {movies.map((movie) => (
-//         <MovieCard key={movie.id} movie={movie} />
-//       ))}
-//     </Stack>
-//   );
-// };
-
-// export default MovieGrid;
-
 import { Stack, Typography } from "@mui/material";
 import useMovies from "../hooks/useMovies";
 import MovieCard from "./MovieCard";
 
 interface MovieGridProps {
-  selectedOption: string | number | undefined; // Define the prop type here
+  selectedOption: string | number | undefined;
 }
 const MovieGrid = ({ selectedOption }: MovieGridProps) => {
   const { movies, error } = useMovies(
