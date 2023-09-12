16ff58460ad8234f1d883d25b74e72c833562569
ddimkou <ddimkou@outlook.com>
2023-09-13 00:03:26 +0300

    display no trailer

--- src/components/MovieDetail/MovieTrailer.tsx
+++ src/components/MovieDetail/MovieTrailer.tsx
@@ -1,9 +1,34 @@
-import { BsPlayCircle } from "react-icons/bs";
+// import { BsPlayCircle } from "react-icons/bs";
+// import { Trailer } from "../../hooks/useTrailerById";
+
+// interface MovieTrailerProps {
+//   trailers: Trailer[];
+// }
+// const MovieTrailer = ({ trailers }: MovieTrailerProps) => {
+//   const trailerVideo = trailers.find((trailer) => trailer.type === "Trailer");
+
+//   const playTrailer = () => {
+//     if (trailerVideo) {
+//       const youtubeUrl = `https://www.youtube.com/watch?v=${trailerVideo.key}`;
+//       window.open(youtubeUrl, "_blank");
+//     }
+//   };
+//   return (
+//     <button className="play-button" onClick={playTrailer}>
+//       <BsPlayCircle size={63} />
+//     </button>
+//   );
+// };
+
+// export default MovieTrailer;
+
+import { BsPlayCircle, BsXCircle } from "react-icons/bs"; // Import the X icon
 import { Trailer } from "../../hooks/useTrailerById";
 
 interface MovieTrailerProps {
   trailers: Trailer[];
 }
+
 const MovieTrailer = ({ trailers }: MovieTrailerProps) => {
   const trailerVideo = trailers.find((trailer) => trailer.type === "Trailer");
 
@@ -13,9 +38,14 @@ const MovieTrailer = ({ trailers }: MovieTrailerProps) => {
       window.open(youtubeUrl, "_blank");
     }
   };
+
   return (
     <button className="play-button" onClick={playTrailer}>
-      <BsPlayCircle size={63} />
+      {trailerVideo ? (
+        <BsPlayCircle size={63} />
+      ) : (
+        <BsXCircle size={63} /> // Render X icon when no trailer is available
+      )}
     </button>
   );
 };
