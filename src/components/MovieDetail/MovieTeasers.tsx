603cff6a17f1c0cf1e46d95461e55860fd71cc9e
ddimkou <ddimkou@outlook.com>
2023-09-13 00:02:06 +0300

    display error if no teasers

--- src/components/MovieDetail/MovieTeasers.tsx
+++ src/components/MovieDetail/MovieTeasers.tsx
@@ -1,3 +1,40 @@
+// import { Box, Typography } from "@mui/material";
+// import { Trailer } from "../../hooks/useTrailerById";
+
+// interface MovieTeasersProps {
+//   trailers: Trailer[];
+// }
+
+// const MovieTeasers = ({ trailers }: MovieTeasersProps) => {
+//   // filter of video is Teaser only
+//   const teaserTrailers = trailers.filter(
+//     (trailer) => trailer.type === "Teaser"
+//   );
+
+//   return (
+//     <Box className="trailers-container" mt={5}>
+//       <Typography variant="h6" textAlign="center" width="100%">
+//         <strong>Teasers</strong>
+//       </Typography>
+//       {teaserTrailers.slice(0, 3).map((trailer) => (
+//         <Box key={trailer.id} className="trailer-card" display="flex">
+//           <iframe
+//             width="280"
+//             height="157.5"
+//             src={`https://www.youtube.com/embed/${trailer.key}`}
+//             title={trailer?.name || ""}
+//             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//             allowFullScreen
+//           ></iframe>
+//           <div className="trailer-title">{trailer.name}</div>
+//         </Box>
+//       ))}
+//     </Box>
+//   );
+// };
+
+// export default MovieTeasers;
+
 import { Box, Typography } from "@mui/material";
 import { Trailer } from "../../hooks/useTrailerById";
 
@@ -6,7 +43,7 @@ interface MovieTeasersProps {
 }
 
 const MovieTeasers = ({ trailers }: MovieTeasersProps) => {
-  // filter of video is Teaser only
+  // Filter videos that are Teasers
   const teaserTrailers = trailers.filter(
     (trailer) => trailer.type === "Teaser"
   );
@@ -16,19 +53,26 @@ const MovieTeasers = ({ trailers }: MovieTeasersProps) => {
       <Typography variant="h6" textAlign="center" width="100%">
         <strong>Teasers</strong>
       </Typography>
-      {teaserTrailers.slice(0, 3).map((trailer) => (
-        <Box key={trailer.id} className="trailer-card" display="flex">
-          <iframe
-            width="280"
-            height="157.5"
-            src={`https://www.youtube.com/embed/${trailer.key}`}
-            title={trailer?.name || ""}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <div className="trailer-title">{trailer.name}</div>
-        </Box>
-      ))}
+
+      {teaserTrailers.length === 0 ? (
+        <Typography variant="body1" textAlign="center">
+          No teasers found 😞
+        </Typography>
+      ) : (
+        teaserTrailers.slice(0, 3).map((trailer) => (
+          <Box key={trailer.id} className="trailer-card" display="flex">
+            <iframe
+              width="280"
+              height="157.5"
+              src={`https://www.youtube.com/embed/${trailer.key}`}
+              title={trailer?.name || ""}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="trailer-title">{trailer.name}</div>
+          </Box>
+        ))
+      )}
     </Box>
   );
 };
