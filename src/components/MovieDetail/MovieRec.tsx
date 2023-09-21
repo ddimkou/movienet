b3230f6b35594d2ae0c23adb991e2464a3ec233e
ddimkou <ddimkou@outlook.com>
2023-09-21 14:20:34 +0300

    limited recommendations

--- src/components/MovieDetail/MovieRec.tsx
+++ src/components/MovieDetail/MovieRec.tsx
@@ -2,23 +2,20 @@ import React from "react";
 import { MovieRecFetch } from "../../hooks/useRecommendation";
 
 interface MovieRecProps {
-  moviesRec: MovieRecFetch | null; // Use the correct type name here
+  moviesRec: MovieRecFetch | null;
 }
 
 const MovieRec = ({ moviesRec }: MovieRecProps) => {
   if (!moviesRec) {
-    // Handle the case where moviesRec is null or undefined
-    return null; // or display a loading message, etc.
+    return null;
   }
 
-  // Now you can map through moviesRec.results and render recommended movies
+  const limitedRec = moviesRec.results.slice(0, 6);
   return (
     <div>
-      {moviesRec.results.map((movie) => (
-        // Render each recommended movie here
+      {limitedRec.map((movie) => (
         <div key={movie.id}>
           <h3>{movie.title}</h3>
-          {/* Add other movie details here */}
         </div>
       ))}
     </div>
