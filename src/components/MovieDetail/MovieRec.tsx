2c0c94c2d7f936e16de182546987291192ad0449
ddimkou <ddimkou@outlook.com>
2023-09-21 14:30:12 +0300

    display card for rec movies

--- src/components/MovieDetail/MovieRec.tsx
+++ src/components/MovieDetail/MovieRec.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { MovieRecFetch } from "../../hooks/useRecommendation";
+import MovieCard from "../MovieCard";
+import { Box, Typography } from "@mui/material";
 
 interface MovieRecProps {
   moviesRec: MovieRecFetch | null;
@@ -11,14 +13,24 @@ const MovieRec = ({ moviesRec }: MovieRecProps) => {
   }
 
   const limitedRec = moviesRec.results.slice(0, 6);
+  console.log(limitedRec);
   return (
-    <div>
-      {limitedRec.map((movie) => (
-        <div key={movie.id}>
-          <h3>{movie.title}</h3>
-        </div>
-      ))}
-    </div>
+    <Box mt={8} mb={20}>
+      <Typography variant="h6" textAlign="center" width="100%">
+        <strong>Recommended Movies</strong>
+      </Typography>
+      <Box
+        display="flex"
+        justifyContent="center"
+        flexWrap="wrap"
+        mt={5}
+        gap={3}
+      >
+        {limitedRec.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </Box>
+    </Box>
   );
 };
 
